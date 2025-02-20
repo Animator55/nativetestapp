a1478cbd1f4b7f4a36ea4280fad53f99e406d6ed
Animator55 <animator55developer@gmail.com>
2025-02-20 00:38:35 -0300

    added default userList to render

---
+++ defaults/userList.js
@@ -0,0 +1,107 @@
+export const userList = [
+    {
+      "_id": "67b643feffc8b48e36156dce",
+      "status": "online",
+      "image": "rgb(182, 226, 40)",
+      "name": "Elnora Butler",
+      "phone": "+1 (879) 412-2815"
+    },
+    {
+      "_id": "67b643fef87832470e45bf23",
+      "status": "offline",
+      "image": "rgb(171, 17, 239)",
+      "name": "Amie Miranda",
+      "phone": "+1 (826) 562-3995"
+    },
+    {
+      "_id": "67b643feeba552d27121ee9c",
+      "status": "offline",
+      "image": "rgb(148, 107, 84)",
+      "name": "Bridget Lawrence",
+      "phone": "+1 (866) 423-2230"
+    },
+    {
+      "_id": "67b643fe4a7a39d11313504b",
+      "status": "online",
+      "image": "rgb(92, 7, 169)",
+      "name": "Teresa Lancaster",
+      "phone": "+1 (907) 446-3465"
+    },
+    {
+      "_id": "67b643fef8d7baf72ddd0667",
+      "status": "online",
+      "image": "rgb(97, 231, 200)",
+      "name": "Lowery Goodman",
+      "phone": "+1 (931) 559-2238"
+    },
+    {
+      "_id": "67b643fee92784a8d58e02a6",
+      "status": "online",
+      "image": "rgb(140, 233, 24)",
+      "name": "Ruiz Mendoza",
+      "phone": "+1 (914) 428-2489"
+    },
+    {
+      "_id": "67b643feb0e050ff472beb81",
+      "status": "online",
+      "image": "rgb(85, 162, 104)",
+      "name": "Mae Delgado",
+      "phone": "+1 (922) 536-2394"
+    },
+    {
+      "_id": "67b643fe5d55e20776b47ab3",
+      "status": "offline",
+      "image": "rgb(168, 131, 207)",
+      "name": "Sims Hernandez",
+      "phone": "+1 (812) 502-3591"
+    },
+    {
+      "_id": "67b643fea77a1ade24d4715b",
+      "status": "offline",
+      "image": "rgb(132, 162, 62)",
+      "name": "Brigitte Daniels",
+      "phone": "+1 (824) 561-2182"
+    },
+    {
+      "_id": "67b643fe36cd66f80ff68882",
+      "status": "offline",
+      "image": "rgb(216, 155, 223)",
+      "name": "Brandi Carroll",
+      "phone": "+1 (803) 507-3858"
+    },
+    {
+      "_id": "67b643fe4f215ea72cec9a94",
+      "status": "offline",
+      "image": "rgb(30, 69, 252)",
+      "name": "Aguilar Talley",
+      "phone": "+1 (846) 410-2841"
+    },
+    {
+      "_id": "67b643fe72c80138de7cd22f",
+      "status": "online",
+      "image": "rgb(222, 28, 3)",
+      "name": "Lupe Garrison",
+      "phone": "+1 (998) 452-3392"
+    },
+    {
+      "_id": "67b643fe3589b43916108de2",
+      "status": "online",
+      "image": "rgb(123, 252, 14)",
+      "name": "Leanna Peterson",
+      "phone": "+1 (942) 598-3399"
+    },
+    {
+      "_id": "67b643fe5e1711a48918a2ea",
+      "status": "online",
+      "image": "rgb(21, 89, 243)",
+      "name": "Woodard Gallagher",
+      "phone": "+1 (878) 519-2668"
+    },
+    {
+      "_id": "67b643fe1737750f608eac62",
+      "status": "offline",
+      "image": "rgb(217, 75, 43)",
+      "name": "Nola Cannon",
+      "phone": "+1 (827) 561-2720"
+    }
+  ]
