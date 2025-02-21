af5d899760e2c1e257404bc0fc9db50d41dfe83f
Animator55 <animator55developer@gmail.com>
2025-02-21 16:54:41 -0300

    Update callList.js

--- defaults/callList.js
+++ defaults/callList.js
@@ -1,17 +1,17 @@
 export const callList = [
-    { name: "Elnora Butler", phone: "+1 (879) 412-2815", image: "rgb(182, 226, 40)", duration: "05:23", timestamp: "20/2/2025", madeBy: "Elnora Butler" },
-    { name: "Amie Miranda", phone: "+1 (826) 562-3995", image: "rgb(171, 17, 239)", duration: "02:47", timestamp: "20/2/2025", madeBy: "You" },
-    { name: "Bridget Lawrence", phone: "+1 (866) 423-2230", image: "rgb(148, 107, 84)", duration: "", timestamp: "20/2/2025", madeBy: "Bridget Lawrence" },
-    { name: "Teresa Lancaster", phone: "+1 (907) 446-3465", image: "rgb(92, 7, 169)", duration: "03:58", timestamp: "19/2/2025", madeBy: "You" },
-    { name: "Lowery Goodman", phone: "+1 (931) 559-2238", image: "rgb(97, 231, 200)", duration: "06:31", timestamp: "19/2/2025", madeBy: "Lowery Goodman" },
-    { name: "Ruiz Mendoza", phone: "+1 (914) 428-2489", image: "rgb(140, 233, 24)", duration: "", timestamp: "18/2/2025", madeBy: "You" },
-    { name: "Mae Delgado", phone: "+1 (922) 536-2394", image: "rgb(85, 162, 104)", duration: "08:44", timestamp: "18/2/2025", madeBy: "Mae Delgado" },
-    { name: "Sims Hernandez", phone: "+1 (812) 502-3591", image: "rgb(168, 131, 207)", duration: "", timestamp: "18/2/2025", madeBy: "You" },
-    { name: "Brigitte Daniels", phone: "+1 (824) 561-2182", image: "rgb(132, 162, 62)", duration: "", timestamp: "17/2/2025", madeBy: "Brigitte Daniels" },
-    { name: "Brandi Carroll", phone: "+1 (803) 507-3858", image: "rgb(216, 155, 223)", duration: "09:05", timestamp: "17/2/2025", madeBy: "You" },
-    { name: "Aguilar Talley", phone: "+1 (846) 410-2841", image: "rgb(30, 69, 252)", duration: "", timestamp: "16/2/2025", madeBy: "Aguilar Talley" },
-    { name: "Lupe Garrison", phone: "+1 (998) 452-3392", image: "rgb(222, 28, 3)", duration: "11:07", timestamp: "16/2/2025", madeBy: "You" },
-    { name: "Leanna Peterson", phone: "+1 (942) 598-3399", image: "rgb(123, 252, 14)", duration: "03:33", timestamp: "15/2/2025", madeBy: "Leanna Peterson" },
-    { name: "Woodard Gallagher", phone: "+1 (878) 519-2668", image: "rgb(21, 89, 243)", duration: "07:14", timestamp: "15/2/2025", madeBy: "You" }
+    { _id: "id_feafaf",name: "Elnora Butler", phone: "+1 (879) 412-2815", image: "rgb(182, 226, 40)", duration: "05:23", timestamp: "20/2/2025", madeBy: "Elnora Butler" },
+    { _id: "id_35tweh",name: "Amie Miranda", phone: "+1 (826) 562-3995", image: "rgb(171, 17, 239)", duration: "02:47", timestamp: "20/2/2025", madeBy: "You" },
+    { _id: "id_htejiv",name: "Bridget Lawrence", phone: "+1 (866) 423-2230", image: "rgb(148, 107, 84)", duration: "", timestamp: "20/2/2025", madeBy: "Bridget Lawrence" },
+    { _id: "id_greuye",name: "Teresa Lancaster", phone: "+1 (907) 446-3465", image: "rgb(92, 7, 169)", duration: "03:58", timestamp: "19/2/2025", madeBy: "You" },
+    { _id: "id_grsh",name: "Lowery Goodman", phone: "+1 (931) 559-2238", image: "rgb(97, 231, 200)", duration: "06:31", timestamp: "19/2/2025", madeBy: "Lowery Goodman" },
+    { _id: "id_grshrs",name: "Ruiz Mendoza", phone: "+1 (914) 428-2489", image: "rgb(140, 233, 24)", duration: "", timestamp: "18/2/2025", madeBy: "You" },
+    { _id: "id_grsdhus",name: "Mae Delgado", phone: "+1 (922) 536-2394", image: "rgb(85, 162, 104)", duration: "08:44", timestamp: "18/2/2025", madeBy: "Mae Delgado" },
+    { _id: "id_gesyhestg",name: "Sims Hernandez", phone: "+1 (812) 502-3591", image: "rgb(168, 131, 207)", duration: "", timestamp: "18/2/2025", madeBy: "You" },
+    { _id: "id_fseyes",name: "Brigitte Daniels", phone: "+1 (824) 561-2182", image: "rgb(132, 162, 62)", duration: "", timestamp: "17/2/2025", madeBy: "Brigitte Daniels" },
+    { _id: "id_gry",name: "Brandi Carroll", phone: "+1 (803) 507-3858", image: "rgb(216, 155, 223)", duration: "09:05", timestamp: "17/2/2025", madeBy: "You" },
+    { _id: "id_grye",name: "Aguilar Talley", phone: "+1 (846) 410-2841", image: "rgb(30, 69, 252)", duration: "", timestamp: "16/2/2025", madeBy: "Aguilar Talley" },
+    { _id: "id_jytuj",name: "Lupe Garrison", phone: "+1 (998) 452-3392", image: "rgb(222, 28, 3)", duration: "11:07", timestamp: "16/2/2025", madeBy: "You" },
+    { _id: "id_ascafs",name: "Leanna Peterson", phone: "+1 (942) 598-3399", image: "rgb(123, 252, 14)", duration: "03:33", timestamp: "15/2/2025", madeBy: "Leanna Peterson" },
+    { _id: "id_fesyheru",name: "Woodard Gallagher", phone: "+1 (878) 519-2668", image: "rgb(21, 89, 243)", duration: "07:14", timestamp: "15/2/2025", madeBy: "You" }
   ];
   
